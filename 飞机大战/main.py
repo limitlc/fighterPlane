8a624004747f7eadc1271701006bb7957cfbfbc6
limitlc <limitlc@163.com>
2021-03-21 10:51:46 +0800

    添加飞机并让飞机能够移动当前飞机不限制边界

--- 飞机大战/main.py
+++ 飞机大战/main.py
@@ -1,24 +1,51 @@
-import pygame
+
 import time
+import pygame
+
+
 # -*- coding: utf-8 -*-
 # @Time    : 2021/3/19 16:31
 # @Author  : limitlc
 # @Email   : limitlc@163.com
+from pygame import *
+
+
 def main():
     screen = pygame.display.set_mode((480, 700), 0, 32)
     background = pygame.image.load("./res/background.png")
-    screen.blit(background, (0, 0))
+
     hero = pygame.image.load('./res/me1.png')
-    screen.blit(hero,(480/2-102/2,700-126))
+
+    heroY = 700 - 126
+    heroX = 480 / 2 - 102 / 2
+
+    herospeed = 1
     while True:
+        screen.blit(background, (0, 0))
+        screen.blit(hero, (heroX, heroY))
         for event in   pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
-        # keppressed = pygame.key.get_pressed()
+        keypressed = pygame.key.get_pressed()
+        if keypressed[K_w] or keypressed[K_UP]:
+            print("上")
+            heroY -= herospeed
+        if keypressed[K_a] or keypressed[K_LEFT]:
+            print("左")
+            heroX -= herospeed
+        if keypressed[K_s] or keypressed[K_DOWN]:
+            print("下")
+            heroY += herospeed
+        if keypressed[K_d] or keypressed[K_RIGHT]:
+            print("右")
+            heroX += herospeed
+        if keypressed[K_SPACE]:
+            print("发射子弹")
+
 
         pygame.display.update()
-        time.sleep(0.01)
+        # time.sleep(0.01)
 
 if __name__ == '__main__':
     main()
